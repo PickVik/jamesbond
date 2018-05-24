2d714bd597ef1633fc96642502c453dc2a937a6f
ViktM <viktoria.mezei0708@gmail.com>
2018-05-24 20:40:50 +0100

    html1

---
+++ html1.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>007 - Sky</title>
+    </head>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+
+    </style>
+    <body>
+        <h1>The features</h1>
+
+        <table>
+
+            <tr>
+
+                <th>Channel</th>
+                <th colspan="2">9 am</th>
+                <th colspan="2">10 am</th>
+                <th colspan="2">11 am</th>
+                <th colspan="2">12 pm</th>
+                <th colspan="2">1 pm</th>
+                <th colspan="2">2 pm</th>
+                <th colspan="2">3 pm</th>
+            </tr>
+
+            <tr>
+                <td>Sean Connery</td>     
+                <td  colspan="4">Dr No</td>
+                <td colspan="5">Thunderball</td>
+                <td colspan="4"></td>
+                
+            </tr>
+
+            <tr>
+                <td>George Lazenby</td>
+                <td colspan="14">No Programmes available</td>
+            </tr>
+
+            <tr>
+                <td>Roger Moore</td>
+                 <td colspan="7">Moonraker</td>
+                <td colspan="7"></td>
+            </tr>
+            <tr>
+                <td>Timothy Dalton</td>
+                <td colspan="14">No Programmes available</td>
+            </tr>
+
+            <tr>
+                <td>Pierce Brosnan</td>
+                <td colspan="14">No Programmes available</td>
+            </tr>
+            <tr>
+                <td>Daniel Craig</td>
+                <td colspan="14">No Programmes available</td>
+            </tr>
+
+
+
+
+
+        </table>
+
+
+
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
